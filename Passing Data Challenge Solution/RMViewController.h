28eb91cfd4a4bcf63567940d90ba2c3e07c317aa
Ryan Macaspac <rmacaspac@gmail.com>
2013-10-29 23:17:38 -0700

    Initial Commit

---
+++ Passing Data Challenge Solution/RMViewController.h
@@ -0,0 +1,13 @@
+//
+//  RMViewController.h
+//  Passing Data Challenge Solution
+//
+//  Created by Ryan Macaspac on 10/29/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RMViewController : UIViewController
+
+@end
---
+++ Passing Data Challenge Solution/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Passing Data Challenge Solution
+//
+//  Created by Ryan Macaspac on 10/29/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RMAppDelegate class]));
+    }
+}
